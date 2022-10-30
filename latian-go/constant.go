69ae7e8f750eb52be2b2aca91b4aa482641808c9
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-10-30 12:46:11 +0700

    go pratice

---
+++ latian-go/constant.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	const (
+		firstName string = "Andreas"
+		lastName         = "Bilmar"
+		age              = 23
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(age)
+
+}
