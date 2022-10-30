416de278e5937e65fbfc2dc11e93138851e9a8a2
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-10-30 11:52:03 +0700

    go pratice

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Andreas")
+	fmt.Println("Andreas Bilmar")
+	fmt.Println("Andreas Bilmar Harteveld Girsang")
+
+	fmt.Println(len("Andreas"))
+	fmt.Println("Andreas Bilmar"[0])
+	fmt.Println("Andreas Bilmar Harteveld Girsang"[1])
+
+}
